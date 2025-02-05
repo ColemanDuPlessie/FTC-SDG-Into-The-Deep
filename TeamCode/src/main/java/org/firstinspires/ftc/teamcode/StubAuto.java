a1affd94176af9ef5de70952cf2deaeca9ce6b05
Lockable-Cape-66 <149549993+Lockable-Cape-66@users.noreply.github.com>
2025-02-05 12:42:26 -0500

    release of claw after clip

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StubAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StubAuto.java
@@ -103,13 +103,14 @@ public class StubAuto extends CommandbasedOpmode {
                 .UNSTABLE_addTemporalMarkerOffset(0.75, () -> robot.wrist.center())
                 .splineToSplineHeading(new Pose2d(STARTX, STARTY*0.3+DEPOSITY*0.7, 0), 0)
                 .waitSeconds(.1)
+                .UNSTABLE_addTemporalMarkerOffset(.5,() -> robot.claw.open())
                 .splineToSplineHeading(new Pose2d(36, -38, 0), 0)// moving to the side
                 .splineToConstantHeading(new Vector2d(32, -18), -CLOCKWISE90*0.45)//getting into position to push
                 .splineToConstantHeading(new Vector2d(36, -10), -90)//in position to push
                 .waitSeconds(3)
                 .lineToSplineHeading(new Pose2d(48, -55, 0))// push to obs
-                .lineToLinearHeading(new Pose2d(48,-45, 90))// back up and turn to collect
-                .waitSeconds(3)
+                .lineToLinearHeading(new Pose2d(48,-45, -CLOCKWISE90))// back up and turn to collect
+                .waitSeconds(1)
                 //lineToLinearHeading(new Pose2d(48,-56,90))
                 .build();
     }
